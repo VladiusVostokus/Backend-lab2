886c00c52b3f9c99f9a5a418a433778a49fcf36a
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-31 19:20:39 +0200

    Validate category endpoints

--- src/categories.py
+++ src/categories.py
@@ -1,22 +1,29 @@
 import uuid
 from flask import Blueprint, request
+from marshmallow import ValidationError
+from schemas import CategorySchema
 
 categories_bp = Blueprint('categories', __name__)
+category_schema = CategorySchema()
+category_list_schema = CategorySchema(many=True)
 categories = {}
 
 @categories_bp.post("/category")
 def add_category():
-    category_data = request.get_json()
+    try:
+        category_data = category_schema.load(request.get_json()) 
+    except ValidationError as err:
+        return err.messages, 400
     category_id = uuid.uuid4().hex
     category = {"id": category_id, **category_data}
     categories[category_id] = category
-    return category
+    return category_schema.dump(category)
 
 @categories_bp.get("/category/<category_id>")
 def get_category(category_id):
     category = categories[category_id]
     if category:
-        return category
+        return category_schema.dump(category)
     return "Category not found", 404
 
 @categories_bp.delete("/category/<category_id>")
@@ -24,5 +31,5 @@ def delete_category(category_id):
     category = categories[category_id]
     if category:
         del categories[category_id]
-        return category
+        return category_schema.dump(category)
     return "Category not found", 404
