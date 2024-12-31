a6f77d63d0b0df246fba3846f92ac302be038dd6
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-31 17:44:26 +0200

    Add marshmallow entities

---
+++ src/schemas.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema, fields
+
+class PlainUserSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+class CategorySchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+class RecordSchema(Schema):
+    id = fields.Str(dump_only=True)
+    user_id = fields.Str(required=False)
+    category_id = fields.Str(required=False)
+    date = fields.Date()
+    expence = fields.Number()
