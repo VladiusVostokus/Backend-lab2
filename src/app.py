f8b6b3fe73621117f37ebe87a660c562517d60d1
VladiusVostokus <kovalenkov222@gmail.com>
2025-01-03 14:26:04 +0200

    Do migration

--- src/app.py
+++ src/app.py
@@ -2,12 +2,16 @@ from flask import Flask
 from users import users_bp
 from categories import categories_bp
 from records import records_bp
+from flask_migrate import Migrate
+from models import db
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py', silent=True)
+app.config.from_pyfile('./config.py', silent=True)
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 app.register_blueprint(users_bp)
 app.register_blueprint(categories_bp)
 app.register_blueprint(records_bp)
-
